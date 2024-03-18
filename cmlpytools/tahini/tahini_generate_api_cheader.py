7c7b2e572b1adc02412139849d5547cb73c85320
Artjom Rjabov <artjom.rjabov@cambridgemechatronics.com>
2024-03-18 13:24:33 +0000

    FW-4098 none string

--- cmlpytools/tahini/tahini_generate_api_cheader.py
+++ cmlpytools/tahini/tahini_generate_api_cheader.py
@@ -178,12 +178,16 @@ class GenerateApiCheader():
         header = HEADER_TEMPLATE.replace("%%YEAR%%", str(year)).replace("%%HEADER_GUARD%%", header_guard)
         footer = FOOTER_TEMPLATE.replace("%%HEADER_GUARD%%", header_guard)
         last_tag = version.git_versions[0].last_tag
+        if (version.uid == None):
+            version_uid = 0
+        else:
+            version_uid = version.uid
         version_string = VERSION_TEMPLATE.replace("%%MAJOR_VERSION%%", str(last_tag.major))\
             .replace("%%MINOR_VERSION%%", str(last_tag.minor))\
             .replace("%%PATCH_VERSION%%", str(last_tag.patch))\
             .replace("%%MAJOR_SUBVERSION%%", str(last_tag.branch_id))\
             .replace("%%MINOR_SUBVERSION%%", str(last_tag.release_num))\
-            .replace("%%UNIQUE_ID%%", str(int(0 or version.uid)))\
+            .replace("%%UNIQUE_ID%%", str(version_uid)\
             .replace("%%BUILDCONFIG_ID%%", str(version.config_id))
 
         output.write(header)
